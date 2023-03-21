ef1edd8703be0d76328fc1e34ace6c0533fe89bf
Rijwan Ansari <rijwan@techoneglobal.com>
2023-03-21 11:48:09 +0800

    Added SAS token generation function and view URL feature

--- RijsatFileUploadAzureStorage/Services/BlobStorageService.cs
+++ RijsatFileUploadAzureStorage/Services/BlobStorageService.cs
@@ -1,5 +1,7 @@
-﻿using Azure.Storage.Blobs;
+﻿using Azure.Storage;
+using Azure.Storage.Blobs;
 using Azure.Storage.Blobs.Models;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System.Reflection.Metadata.Ecma335;
 
@@ -12,7 +14,7 @@ public class BlobStorageService : IBlobStorageService
     private string blobContainerName = "rijsatdemo";
     public BlobStorageService(IConfiguration configuration, ILogger<BlobStorageService> logger)
     {
-        _configuration = configuration;   
+        _configuration = configuration;
         _logger = logger;
         blobStorageconnection = _configuration.GetConnectionString("AzureStorageAccount");
     }
@@ -48,11 +50,36 @@ public class BlobStorageService : IBlobStorageService
             await blob.DeleteIfExistsAsync(DeleteSnapshotsOption.IncludeSnapshots);
             return true;
         }
-        catch (Exception ex )
+        catch (Exception ex)
         {
             _logger?.LogError(ex.ToString());
             throw;
         }
     }
+
+    public async Task<string> GetBlobSASTOkenByFile(string fileName)
+    {
+        try
+        {
+            var azureStorageAccount = _configuration.GetSection("AzureStorage:AzureAccount").Value;
+            var azureStorageAccessKey = _configuration.GetSection("AzureStorage:AccessKey").Value;
+            Azure.Storage.Sas.BlobSasBuilder blobSasBuilder = new Azure.Storage.Sas.BlobSasBuilder()
+            {
+                BlobContainerName = blobContainerName,
+                BlobName = fileName,
+                ExpiresOn = DateTime.UtcNow.AddMinutes(2),//Let SAS token expire after 5 minutes.
+            };
+            blobSasBuilder.SetPermissions(Azure.Storage.Sas.BlobSasPermissions.Read);//User will only be able to read the blob and it's properties
+            var sasToken = blobSasBuilder.ToSasQueryParameters(new StorageSharedKeyCredential(azureStorageAccount,
+                azureStorageAccessKey)).ToString();
+            return sasToken;
+        }
+        catch (Exception)
+        {
+
+            throw;
+        }
+    }
+
 }
 
--- RijsatFileUploadAzureStorage/Services/IBlobStorageService.cs
+++ RijsatFileUploadAzureStorage/Services/IBlobStorageService.cs
@@ -3,4 +3,5 @@ public interface IBlobStorageService
 {
     Task<string> UploadFileToBlobAsync(string strFileName, string contecntType, Stream fileStream);
     Task<bool> DeleteFileToBlobAsync(string strFileName);
+    Task<string> GetBlobSASTOkenByFile(string fileName);
 }
